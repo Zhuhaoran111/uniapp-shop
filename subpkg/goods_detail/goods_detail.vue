4414a58696e662b0b02453b1292c978b4dc3d8ef
zhuhaoran <zhuhaoran@ambition.com>
2022-07-24 17:43:07 +0800

    商品详情完成

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,43 @@
 <template>
-	<view>
-		商品详情页
+	<view v-if=goods_info.goods_price class="goods_detail-container">
+			<!-- 1、轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item v-for="(item,index) in goods_info.pics" :key="index">
+				<image :src="item.pics_big" @click="preview(index)" ></image>
+			</swiper-item>
+		</swiper>
+		
+		<!-- 2、商品的参数信息区域 -->
+		<view class="goods-info-box">
+			<!-- 商品的价格 -->
+			<view class="price">￥{{goods_info.goods_price}}</view>
+			
+			<!-- 商品信息的主体区域 -->
+			<view class="goods-info-body">
+				<!-- 商品的名字 -->
+				<view class="goods-name">
+					{{goods_info.goods_name}}
+				</view>
+				<!-- 收藏 -->
+				<view class="favi">
+					<uni-icons type="star" size="18" color="gray" ></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			
+			<!-- 运费 -->
+			  <view class="yf">
+			  	快递:免运费
+			  </view>
+		</view>
+		
+		<!-- 3、富文本区域 :nodes是把html结构转换-->
+		<rich-text :nodes="goods_info.goods_introduce"></rich-text>
+		
+		<!-- 4、商品底部导航区域 -->
+		<view class="goods_nav">
+			<uni-goods-nav :fill="true"  :options="options" :buttonGroup="buttonGroup"  @click="onClick" @buttonClick="buttonClick" />
+		</view>
 	</view>
 </template>
 
@@ -8,12 +45,118 @@
 	export default {
 		data() {
 			return {
+				  options: [ {
+							icon: 'shop',
+							text: '店铺',
+							
+							infoBackgroundColor:'#007aff',
+							infoColor:"red"
+						}, {
+							icon: 'cart',
+							text: '购物车',
+							info: 9
+						}],
+					    buttonGroup: [{
+					      text: '加入购物车',
+					      backgroundColor: '#ff0000',
+					      color: '#fff'
+					    },
+					    {
+					      text: '立即购买',
+					      backgroundColor: '#ffa200',
+					      color: '#fff'
+					    }
+					    ],
 				
+				goods_info: {}// 商品详情对象
 			};
+		},
+		onLoad(options) {	
+		const goods_id = options.goods_id// 获取商品 Id	
+		 this.getGoodsDetail(goods_id)// 调用请求商品详情数据的方法
+		},
+		
+		methods:{
+			async getGoodsDetail(goods_id){
+		    const { data: res } = await uni.$http.get('/api/public/v1/goods/detail', { goods_id })
+			if (res.meta.status !== 200) return uni.$showMsg()
+			// 为 data 中的数据赋值
+			
+			// 使用字符串的 replace() 方法，为 img 标签添加行内的 style 样式，从而解决图片底部
+			//空白间隙的问题（正则替换)
+			res.message.goods_introduce = res.message.goods_introduce.replace(/<img/g, '<img style="display:block;" ')
+			this.goods_info = res.message
+			},
+			
+			//轮播图片预览效果展示uni.previewImage大图预览
+			preview(index){
+				uni.previewImage({
+					current:index,
+					urls:this.goods_info.pics.map(x=>x.pics_big)  //用map遍历地址
+				})
+			},
+			
+			//点击底部左侧区域的处理函数
+			onClick(e){
+				if(e.content.text==='购物车'){
+					uni.switchTab({
+						url:'/pages/cart/cart'
+					})				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	.goods_detail-container{
+		padding-bottom:50px
+	}
+swiper{
+	height: 750rpx;
+	image{
+		width: 100%;
+		height:100%;
+	}
+}
 
+.goods-info-box{
+	padding:10px;
+	padding-right:0;
+	.price{
+		color:#c00000;
+		font-size: 18px;
+		margin:10px 0;
+	}
+	
+	.goods-info-body{
+		display: flex;
+		justify-content: space-between;
+		.goods-name{
+			font-size: 13px;
+			margin-right:10px
+		}
+		.favi{
+			width: 120px;
+			font-size: 12px;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			border-left:1px solid #efefef;
+			color:gray
+		}
+	}
+	
+	.yf{
+		font-size: 12px;
+		color:gray;
+		margin:10px 0;
+	}
+}
+
+.goods_nav{
+	position: fixed;
+	bottom:0;
+	left:0;
+	width:100%;
+}
 </style>
--- unpackage/dist/dev/mp-weixin/common/vendor.js
+++ unpackage/dist/dev/mp-weixin/common/vendor.js
@@ -2675,6 +2675,57 @@ var $http = new Request();exports.$http = $http;
 
 /***/ }),
 
+/***/ 121:
+/*!***************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-goods-nav/components/uni-goods-nav/i18n/index.js ***!
+  \***************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _en = _interopRequireDefault(__webpack_require__(/*! ./en.json */ 122));
+var _zhHans = _interopRequireDefault(__webpack_require__(/*! ./zh-Hans.json */ 123));
+var _zhHant = _interopRequireDefault(__webpack_require__(/*! ./zh-Hant.json */ 124));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var _default =
+{
+  en: _en.default,
+  'zh-Hans': _zhHans.default,
+  'zh-Hant': _zhHant.default };exports.default = _default;
+
+/***/ }),
+
+/***/ 122:
+/*!**************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-goods-nav/components/uni-goods-nav/i18n/en.json ***!
+  \**************************************************************************************************/
+/*! exports provided: uni-goods-nav.options.shop, uni-goods-nav.options.cart, uni-goods-nav.buttonGroup.addToCart, uni-goods-nav.buttonGroup.buyNow, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-goods-nav.options.shop\":\"shop\",\"uni-goods-nav.options.cart\":\"cart\",\"uni-goods-nav.buttonGroup.addToCart\":\"add to cart\",\"uni-goods-nav.buttonGroup.buyNow\":\"buy now\"}");
+
+/***/ }),
+
+/***/ 123:
+/*!*******************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-goods-nav/components/uni-goods-nav/i18n/zh-Hans.json ***!
+  \*******************************************************************************************************/
+/*! exports provided: uni-goods-nav.options.shop, uni-goods-nav.options.cart, uni-goods-nav.buttonGroup.addToCart, uni-goods-nav.buttonGroup.buyNow, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-goods-nav.options.shop\":\"店铺\",\"uni-goods-nav.options.cart\":\"购物车\",\"uni-goods-nav.buttonGroup.addToCart\":\"加入购物车\",\"uni-goods-nav.buttonGroup.buyNow\":\"立即购买\"}");
+
+/***/ }),
+
+/***/ 124:
+/*!*******************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-goods-nav/components/uni-goods-nav/i18n/zh-Hant.json ***!
+  \*******************************************************************************************************/
+/*! exports provided: uni-goods-nav.options.shop, uni-goods-nav.options.cart, uni-goods-nav.buttonGroup.addToCart, uni-goods-nav.buttonGroup.buyNow, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-goods-nav.options.shop\":\"店鋪\",\"uni-goods-nav.options.cart\":\"購物車\",\"uni-goods-nav.buttonGroup.addToCart\":\"加入購物車\",\"uni-goods-nav.buttonGroup.buyNow\":\"立即購買\"}");
+
+/***/ }),
+
 /***/ 19:
 /*!**********************************************************!*\
   !*** ./node_modules/@babel/runtime/regenerator/index.js ***!
@@ -10022,58 +10073,7 @@ internalMixin(Vue);
 
 /***/ }),
 
-/***/ 76:
-/*!*****************************************************************************************************!*\
-  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/index.js ***!
-  \*****************************************************************************************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _en = _interopRequireDefault(__webpack_require__(/*! ./en.json */ 77));
-var _zhHans = _interopRequireDefault(__webpack_require__(/*! ./zh-Hans.json */ 78));
-var _zhHant = _interopRequireDefault(__webpack_require__(/*! ./zh-Hant.json */ 79));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var _default =
-{
-  en: _en.default,
-  'zh-Hans': _zhHans.default,
-  'zh-Hant': _zhHant.default };exports.default = _default;
-
-/***/ }),
-
-/***/ 77:
-/*!****************************************************************************************************!*\
-  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/en.json ***!
-  \****************************************************************************************************/
-/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
-/***/ (function(module) {
-
-module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"Search enter content\"}");
-
-/***/ }),
-
 /***/ 78:
-/*!*********************************************************************************************************!*\
-  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hans.json ***!
-  \*********************************************************************************************************/
-/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
-/***/ (function(module) {
-
-module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"请输入搜索内容\"}");
-
-/***/ }),
-
-/***/ 79:
-/*!*********************************************************************************************************!*\
-  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hant.json ***!
-  \*********************************************************************************************************/
-/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
-/***/ (function(module) {
-
-module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"請輸入搜索內容\"}");
-
-/***/ }),
-
-/***/ 87:
 /*!**************************************************************************************!*\
   !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-icons/components/uni-icons/icons.js ***!
   \**************************************************************************************/
@@ -11247,6 +11247,57 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
     "font_class": "cart",
     "unicode": "e631",
     "unicode_decimal": 58929 }] };exports.default = _default;
+
+/***/ }),
+
+/***/ 93:
+/*!*****************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/index.js ***!
+  \*****************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _en = _interopRequireDefault(__webpack_require__(/*! ./en.json */ 94));
+var _zhHans = _interopRequireDefault(__webpack_require__(/*! ./zh-Hans.json */ 95));
+var _zhHant = _interopRequireDefault(__webpack_require__(/*! ./zh-Hant.json */ 96));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}var _default =
+{
+  en: _en.default,
+  'zh-Hans': _zhHans.default,
+  'zh-Hant': _zhHant.default };exports.default = _default;
+
+/***/ }),
+
+/***/ 94:
+/*!****************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/en.json ***!
+  \****************************************************************************************************/
+/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"Search enter content\"}");
+
+/***/ }),
+
+/***/ 95:
+/*!*********************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hans.json ***!
+  \*********************************************************************************************************/
+/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"请输入搜索内容\"}");
+
+/***/ }),
+
+/***/ 96:
+/*!*********************************************************************************************************!*\
+  !*** H:/uniapp/易购商城/uniapp-shop/uni_modules/uni-search-bar/components/uni-search-bar/i18n/zh-Hant.json ***!
+  \*********************************************************************************************************/
+/*! exports provided: uni-search-bar.cancel, uni-search-bar.placeholder, default */
+/***/ (function(module) {
+
+module.exports = JSON.parse("{\"uni-search-bar.cancel\":\"cancel\",\"uni-search-bar.placeholder\":\"請輸入搜索內容\"}");
 
 /***/ })
 
